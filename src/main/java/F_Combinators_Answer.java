77a0b8cff33ca8cb76ac10fa3ab28fe92586798e
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-18 19:34:38 +0200

    combinators - vavr collections

--- src/main/java/F_Combinators_Answer.java
+++ src/main/java/F_Combinators_Answer.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import io.vavr.collection.List;
+
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -7,11 +8,11 @@ public class F_Combinators_Answer {
 
     static <A, R> R reduce(List<A> xs, R zero, BiFunction<R, A, R> combine) {
         if (xs.isEmpty()) return zero;
-        else return combine.apply(reduce(Lists.tail(xs), zero, combine), Lists.head(xs));
+        else return combine.apply(reduce(xs.tail(), zero, combine), xs.head());
     }
 
     static <A, B> List<B> flatMap(List<A> xs, Function<A, List<B>> f) {
-        return reduce(xs, List.of(), (acc, x) -> Lists.concat(f.apply(x), acc));
+        return reduce(xs, List.of(), (acc, x) -> f.apply(x).appendAll(acc));
     }
 
     static <A> List<A> filter(List<A> xs, Predicate<A> f) {
@@ -24,7 +25,7 @@ public class F_Combinators_Answer {
 
     static List<Integer> iterate(int max) {
         if (max == 0) return List.of();
-        else return Lists.append(iterate(max - 1), max);
+        else return iterate(max - 1).append(max);
     }
 
     static List<Integer> filterEven(List<Integer> xs) {
