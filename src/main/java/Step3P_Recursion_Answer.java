b15149db0be5fe3677ef7367dc92e1307f783912
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-18 22:56:43 +0200

    creation of additional step for vavr functionalities

---
+++ src/main/java/Step3P_Recursion_Answer.java
@@ -0,0 +1,31 @@
+import io.vavr.collection.List;
+
+public class Step3P_Recursion_Answer {
+
+    static List<Integer> iteratePositiveUpTo(int max) {
+        return max <= 0
+                ? List.empty()
+                : iteratePositiveUpTo(max - 1).append(max);
+    }
+
+    static List<Integer> filterEven(List<Integer> xs) {
+        if (xs.isEmpty()) return List.empty();
+        else return xs.head() % 2 == 0
+                ? filterEven(xs.tail()).prepend(xs.head())
+                : filterEven(xs.tail());
+    }
+
+    static List<Integer> square(List<Integer> xs) {
+        if (xs.isEmpty()) return List.empty();
+        else return square(xs.tail()).prepend(xs.head() * xs.head());
+    }
+
+    static int sum(List<Integer> xs) {
+        if (xs.isEmpty()) return 0;
+        else return xs.head() + sum(xs.tail());
+    }
+
+    static int sumOfSquaresOfPositiveEvenNumbersUpTo(int max) {
+        return sum(square(filterEven(iteratePositiveUpTo(max))));
+    }
+}
