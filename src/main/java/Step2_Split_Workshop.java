7a23ae718b521a516bfbff10007020cdcb548aa0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-18 21:02:59 +0200

    Step2_Split_Workshop.java

---
+++ src/main/java/Step2_Split_Workshop.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Step2_Split_Workshop {
+
+     static List<Integer> iteratePositiveUpTo(int max) {
+        // closed interval [1, max]
+         
+         return null;
+    }
+
+     static List<Integer> filterEven(List<Integer> xs) {
+        return null;
+    }
+
+     static List<Integer> square(List<Integer> xs) {
+        return null;
+    }
+
+     static int sum(List<Integer> xs) {
+        return 0;
+    }
+
+    static int sumOfSquaresOfEvenPositiveNumbersUpTo(int max) {
+        return sum(square(filterEven(iteratePositiveUpTo(max))));
+    }
+}
