aba54b3c2b61f4799c4029f0bfbc49fc598b8e42
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-18 19:24:26 +0200

    recursion - vavr collections

--- src/main/java/C_Recursion_Answer.java
+++ src/main/java/C_Recursion_Answer.java
@@ -1,27 +1,28 @@
-import java.util.List;
+import io.vavr.collection.List;
 
 public class C_Recursion_Answer {
 
     static List<Integer> iterate(int max) {
-        if (max == 0) return List.of();
-        else return Lists.append(iterate(max - 1), max);
+        return max == 0
+                ? List.empty()
+                : iterate(max - 1).append(max);
     }
 
     static List<Integer> filterEven(List<Integer> xs) {
         if (xs.isEmpty()) return List.of();
-        else return Lists.head(xs) % 2 == 0 
-                ? Lists.prepend(Lists.head(xs), filterEven(Lists.tail(xs))) 
-                : filterEven(Lists.tail(xs));
+        else return xs.head() % 2 == 0
+                ? filterEven(xs.tail()).prepend(xs.head())
+                : filterEven(xs.tail());
     }
 
     static List<Integer> square(List<Integer> xs) {
         if (xs.isEmpty()) return List.of();
-        else return Lists.prepend(Lists.head(xs) * Lists.head(xs), square(Lists.tail(xs)));
+        else return square(xs.tail()).prepend(xs.head() * xs.head());
     }
 
     static int sum(List<Integer> xs) {
         if (xs.isEmpty()) return 0;
-        else return Lists.head(xs) + sum(Lists.tail(xs));
+        else return xs.head() + sum(xs.tail());
     }
 
     static int sumOfSquaresOfEvenNumbersUpTo(int max) {
