38d3f1782bf0cc6ccd7ff9e09f23754308123fac
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-18 19:09:55 +0200

    Lists refactor

--- src/main/java/Lists.java
+++ src/main/java/Lists.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class Lists {
-    public static <A> List<A> prepend(A x, List<A> xs) {
-        var result = new ArrayList<A>(xs);
-        result.add(0, x);
+class Lists {
+    static <A> List<A> prepend(A x, List<A> xs) {
+        var result = new LinkedList<>(xs);
+        result.addFirst(x);
         return result;
     }
 
-    public static <A> List<A> append(List<A> xs, A x) {
-        var result = new ArrayList<A>(xs);
-        result.add(x);
+    static <A> List<A> append(List<A> xs, A x) {
+        var result = new LinkedList<>(xs);
+        result.addFirst(x);
         return result;
     }
 
-    public static <A> List<A> tail(List<A> xs) {
+    static <A> List<A> tail(List<A> xs) {
         return xs.size() <= 1 ? List.of() : xs.subList(1, xs.size());
     }
 
-    public static <A> A head(List<A> xs) {
+    static <A> A head(List<A> xs) {
         return xs.get(0);
     }
 
-    public static <A> List<A> concat(List<A> a, List<A> b) {
-        var result = new ArrayList<A>(a);
+    static <A> List<A> concat(List<A> a, List<A> b) {
+        var result = new ArrayList<>(a);
         result.addAll(b);
         return result;
     }
