1e575b1ec12a4e2c075f385a1ed76d8988b78099
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-06-19 23:20:20 +0200

    Step3P_Recursion_Workshop.java + tests

---
+++ src/main/java/workshop/Step3P_Recursion_Workshop.java
@@ -0,0 +1,22 @@
+package workshop;
+
+import io.vavr.collection.List;
+
+public class Step3P_Recursion_Workshop {
+    
+    static List<Integer> square(List<Integer> xs) {
+        // xs.headOption()
+        // map
+        // getOrElse
+        return null;
+    }
+
+    static int sum(List<Integer> xs) {
+        // xs.headOption()
+        // map
+        // getOrElse
+        
+        return -1;
+    }
+    
+}
---
+++ src/test/groovy/workshop/Step3P_Recursion_WorkshopTest.groovy
@@ -0,0 +1,47 @@
+package workshop
+
+
+import io.vavr.collection.List
+import spock.lang.Specification 
+/**
+ * Created by mtumilowicz on 2019-06-18.
+ */
+class Step3P_Recursion_WorkshopTest extends Specification {
+
+    def "test square < 0"() {
+        given:
+        def negatives = List.of(-4, -3, -2, -1)
+        
+        expect:
+        Step3P_Recursion_Workshop.square(negatives) == List.of(16, 9, 4, 1)
+    }    
+    
+    def "test square 0"() {
+        given:
+        def negatives = List.of(0)
+
+        expect:
+        Step3P_Recursion_Workshop.square(negatives) == List.of(0)
+    }    
+    
+    def "test square > 0"() {
+        given:
+        def negatives = List.of(4, 3, 2, 1)
+
+        expect:
+        Step3P_Recursion_Workshop.square(negatives) == List.of(16, 9, 4, 1)
+    }
+
+    def "test square mixed"() {
+        given:
+        def negatives = List.of(-1, 2)
+
+        expect:
+        Step3P_Recursion_Workshop.square(negatives) == List.of(1, 4)
+    }
+
+    def "test sum"() {
+        expect:
+        Step3P_Recursion_Workshop.sum(List.of(-1, 1, 2, 3, -4, 5)) == 6
+    }
+}
